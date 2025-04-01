b23c06703d75ed5a1fbc15fca88c29fa7c967f13
Keith Khadar <keith.khadar@gmail.com>
2025-04-01 18:11:31 +0000

    Added Nvim install scripts

---
+++ install.sh
@@ -0,0 +1,29 @@
+# Change to the directory where this script is located
+cd "$(dirname "$0")" || { echo "Failed to change directory. Exiting."; exit 1; }
+
+echo "Now in the script's directory: $(pwd)"
+
+# Install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# Add nvim to the path
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+# Clean up (remove the tarbell)
+sudo rm -f ./nvim-linux-x86_64.tar.gz
+
+# Install Neovim config based off NvChad
+
+sudo apt install xclip -y
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.local/share/nvim
+mkdir ~/.config/nvim
+mv ./{*,.*} ~/.config/nvim/
+
+nvim --headless +"MasonInstall" +qall # joe is the most awesome
+

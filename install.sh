7e6aac4ad84de9b7993e49f38da639acd42885c7
keith khadar <keith.khadar@gmail.com>
2025-04-02 20:22:31 +0000

    before installing nvim make sure its not installed

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ cd "$(dirname "$0")" || { echo "Failed to change directory. Exiting."; exit 1; }
 echo "Now in the script's directory: $(pwd)"
 
 # Install nvim
+# Just to be safe also make sure neovim isnt installed by apt
+sudo apt remove nvim -y
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
@@ -22,6 +24,7 @@ sudo apt install xclip -y
 rm -rf ~/.config/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
 mkdir ~/.config/nvim -p
 mv ./{*,.*} ~/.config/nvim/
 
